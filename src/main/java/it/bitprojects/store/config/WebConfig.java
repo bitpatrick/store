e39582813dc8feebe2a3269c8b165e74c7860f9b
p.cervigni <p.cervigni@prismaprogetti.it>
2023-05-16 17:41:30 +0200

    Commit Iniziale

---
+++ src/main/java/it/bitprojects/store/config/WebConfig.java
@@ -0,0 +1,22 @@
+package it.bitprojects.store.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "it.bitprojects.store.controller")
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		registry.viewResolver(resolver);
+	}
+
+}
---
+++ src/main/java/it/bitprojects/store/controller/TestController.java
@@ -0,0 +1,34 @@
+package it.bitprojects.store.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	ApplicationContext context;
+
+	@RequestMapping(value = "/test")
+	@ResponseBody
+	public String handleRequest() {
+
+		Map<String, HandlerMapping> matchingBeans = BeanFactoryUtils.beansOfTypeIncludingAncestors(context,
+				HandlerMapping.class, true, false);
+
+		matchingBeans.forEach((k, v) -> System.out.printf("order:%s %s=%s%n", ((Ordered) v).getOrder(), k,
+				v.getClass().getSimpleName()));
+		
+		return "response from /test";
+
+	}
+
+}
