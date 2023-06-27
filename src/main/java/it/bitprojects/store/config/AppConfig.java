2e06e3f92b1745734e856d3ee9bdee2d540c7db8
p.cervigni <p.cervigni@prismaprogetti.it>
2023-06-27 16:51:04 +0200

    COMMENTO PATRIZIO

--- src/main/java/it/bitprojects/store/config/AppConfig.java
+++ src/main/java/it/bitprojects/store/config/AppConfig.java
@@ -5,12 +5,15 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
+
+/*
+ * PATRIZIO
+ */
 
 @Configuration
 @ComponentScan(basePackages = { "it.bitprojects.store.model", "it.bitprojects.store.repository",
