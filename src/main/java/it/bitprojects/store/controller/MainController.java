067bcc02c09df4740da5d69c70276b0fac921381
p.cervigni <p.cervigni@prismaprogetti.it>
2023-07-04 16:04:38 +0200

    working in progress

--- src/main/java/it/bitprojects/store/controller/MainController.java
+++ src/main/java/it/bitprojects/store/controller/MainController.java
@@ -3,12 +3,12 @@ package it.bitprojects.store.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -57,24 +57,22 @@ public class MainController {
 		return "purchase";
 	}
 
-	@PostMapping(value = "/addProduct", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String addProduct(@RequestBody MultiValueMap<String, String> formParams) {
-		
-		String idProduct = formParams.getFirst("idProduct");
-	    String qty = formParams.getFirst("qty");
+	@PostMapping(value = "/cart-addProduct/{idProduct}")
+	public String addProduct(@PathVariable("idProduct") Integer idProduct, @RequestBody MultiValueMap<String, String> formParams) {
 
-	    // convert strings to integers
-	    int idProductInt = Integer.parseInt(idProduct);
-	    int qtyInt = Integer.parseInt(qty);
+		String qty = formParams.getFirst("quantity");
 
-	    // recupero carrello
-	    Cart cart = this.cartProvider.get();
-	    
-	    //verificare giacenza magazzino
-	    this.store.addProductToCart(idProductInt, qtyInt, cart);
+		// convert strings to integers
+		int qtyInt = Integer.parseInt(qty);
 
-	    // restituisci la lista
-		return "home";
+		// recupero carrello
+		Cart cart = this.cartProvider.get();
+
+		// verificare giacenza magazzino
+		this.store.addProductToCart(idProduct, qtyInt, cart);
+
+		// restituisci la lista
+		return "redirect:/home";
 	}
 
 	@ModelAttribute("cart")
